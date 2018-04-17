88459f5648de3711b7b4cf26e9131871ca9b7e51
ruccin <38451763+ruccin@users.noreply.github.com>
2018-04-17 15:29:40 +0900

    Add files via upload

---
+++ lena-cr-test.cc
@@ -0,0 +1,117 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2011 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Manuel Requena <manuel.requena@cttc.es>
+ *         Nicola Baldo <nbaldo@cttc.es>
+ */
+
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/lte-module.h"
+#include "ns3/config-store.h"
+#include "ns3/radio-bearer-stats-calculator.h"
+
+#include <iomanip>
+#include <string>
+
+using namespace ns3;
+
+int main (int argc, char *argv[])
+{
+
+  uint8_t bandwidth = 25;
+  double simTime = 3.0;
+
+  CommandLine cmd;
+  cmd.AddValue ("radius", "the radius of the disc where UEs are placed around an eNB", radius);
+  cmd.AddValue ("simTime", "Total duration of the simulation (in seconds)", simTime);
+  cmd.Parse (argc, argv);
+
+  // parse again so you can override default values from the command line
+  cmd.Parse (argc, argv);
+
+  IntegerValue runValue;
+  GlobalValue::GetValueByName ("RngRun", runValue);
+
+  Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+  
+  lteHelper->SetEnbDeviceAttribute ("DlBandwidth", UintegerValue (bandwidth));
+  lteHelper->SetEnbDeviceAttribute ("UlBandwidth", UintegerValue (bandwidth));
+  
+  // Create Nodes: eNodeB and UE
+  NodeContainer enbNodes;
+  NodeContainer ueNodes1, ueNodes2;
+  enbNodes.Create (2);
+  ueNodes1.Create (1);
+  ueNodes2.Create (1);
+
+  // Position of eNBs
+  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
+  positionAlloc->Add (Vector (0.866, 0.1, 0.0));
+  positionAlloc->Add (Vector (0.5, 0.4, 0.0));
+  MobilityHelper enbMobility;
+  enbMobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  enbMobility.SetPositionAllocator (positionAlloc);
+  enbMobility.Install (enbNodes);
+
+  // Position of UEs attached to eNB 1
+  MobilityHelper ue1mobility;
+  ue1mobility.SetPositionAllocator ("ns3::UniformDiscPositionAllocator",
+                                    "X", DoubleValue (0.0),
+                                    "Y", DoubleValue (0.0),
+                                    "rho", DoubleValue (50));
+  ue1mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  ue1mobility.Install (ueNodes1);
+
+  // Position of UEs attached to eNB 2
+  MobilityHelper ue2mobility;
+  ue2mobility.SetPositionAllocator ("ns3::UniformDiscPositionAllocator",
+                                    "X", DoubleValue (0.3),
+                                    "Y", DoubleValue (0.3),
+                                    "rho", DoubleValue (80));
+  ue2mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  ue2mobility.Install (ueNodes2);
+
+  // Create Devices and install them in the Nodes (eNB and UE)
+  NetDeviceContainer enbDevs;
+  NetDeviceContainer ueDevs1;
+  NetDeviceContainer ueDevs2;
+  enbDevs = lteHelper->InstallEnbDevice (enbNodes);
+  ueDevs1 = lteHelper->InstallUeDevice (ueNodes1);
+  ueDevs2 = lteHelper->InstallUeDevice (ueNodes2);
+
+  // Attach UEs to a eNB
+  lteHelper->Attach (ueDevs1, enbDevs.Get (0));
+  lteHelper->Attach (ueDevs2, enbDevs.Get (1));
+
+  // Activate a data radio bearer each UE
+  enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
+  EpsBearer bearer (q);
+  lteHelper->ActivateDataRadioBearer (ueDevs1, bearer);
+  lteHelper->ActivateDataRadioBearer (ueDevs2, bearer);
+
+  Simulator::Stop (Seconds (simTime));
+
+  lteHelper->EnableMacTraces ();
+  lteHelper->EnableRlcTraces ();
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+}
