46d3645b60c8af97a411d9a5f68d488598473e51
ruccin <hecate4428@daum.net>
2018-04-24 14:59:05 +0900

    fixed

--- lena-test.cc
+++ lena-test.cc
@@ -168,7 +168,7 @@ main (int argc, char *argv[])
   // Install and start applications on UEs and remote host
   uint16_t dlPort = 1234;
   uint16_t ulPort = 2000;
-  //uint16_t otherPort = 3000;
+  uint16_t otherPort = 3000;
   ApplicationContainer clientApps;
   ApplicationContainer serverApps;
   for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
@@ -177,10 +177,10 @@ main (int argc, char *argv[])
       //++otherPort;
       PacketSinkHelper dlPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), dlPort));
       PacketSinkHelper ulPacketSinkHelper ("ns3::udpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), ulPort));
-      //PacketSinkHelper packetSinkHelper ("ns3::udpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), otherPort));
+      PacketSinkHelper packetSinkHelper ("ns3::udpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), otherPort));
       serverApps.Add (dlPacketSinkHelper.Install (ueNodes.Get(u)));
       serverApps.Add (ulPacketSinkHelper.Install (remoteHost));
-      //serverApps.Add (packetSinkHelper.Install (ueNodes.Get(u)));
+      serverApps.Add (packetSinkHelper.Install (ueNodes.Get(u)));
       
       UdpClientHelper dlClient (ueIpIface.GetAddress (u), dlPort);
       dlClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
@@ -190,16 +190,13 @@ main (int argc, char *argv[])
       ulClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
       ulClient.SetAttribute ("MaxPackets", UintegerValue(1000000));
       
-      /*
       UdpClientHelper client (ueIpIface.GetAddress (u), otherPort);
       client.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
       client.SetAttribute ("MaxPackets", UintegerValue(1000000));
-      */
 
       clientApps.Add (dlClient.Install (remoteHost));
       clientApps.Add (ulClient.Install (ueNodes.Get(u)));
       
-      /*
       if (u+1 < ueNodes.GetN ())
         {
           clientApps.Add (client.Install (ueNodes.Get(u+1)));
@@ -208,7 +205,6 @@ main (int argc, char *argv[])
         {
           clientApps.Add (client.Install (ueNodes.Get(0)));
         }
-      */
     }
   serverApps.Start (Seconds (0.01));
   clientApps.Start (Seconds (0.01));
