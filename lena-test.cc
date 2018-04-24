50196afae418ec92af9897d52b79c7229417da2a
ruccin <hecate4428@daum.net>
2018-04-24 13:41:43 +0900

    fixed

--- lena-test.cc
+++ lena-test.cc
@@ -174,10 +174,10 @@ main (int argc, char *argv[])
       ++otherPort;
       PacketSinkHelper dlPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), dlPort));
       PacketSinkHelper ulPacketSinkHelper ("ns3::udpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), ulPort));
-      //PacketSinkHelper packetSinkHelper ("ns3::udpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), otherPort));
+      PacketSinkHelper packetSinkHelper ("ns3::udpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), otherPort));
       serverApps.Add (dlPacketSinkHelper.Install (ueNodes.Get(u)));
       serverApps.Add (ulPacketSinkHelper.Install (remoteHost));
-      //serverApps.Add (packetSinkHelper.Install (ueNodes.Get(u)));
+      serverApps.Add (packetSinkHelper.Install (ueNodes.Get(u)));
       
       UdpClientHelper dlClient (ueIpIface.GetAddress (u), dlPort);
       dlClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
@@ -187,16 +187,15 @@ main (int argc, char *argv[])
       ulClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
       ulClient.SetAttribute ("MaxPackets", UintegerValue(1000000));
       
-
-      /*
+      
       UdpClientHelper client (ueIpIface.GetAddress (u), otherPort);
       client.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
       client.SetAttribute ("MaxPackets", UintegerValue(1000000));
-      */
+      
 
       clientApps.Add (dlClient.Install (remoteHost));
       clientApps.Add (ulClient.Install (ueNodes.Get(u)));
-      /*
+      
       if (u+1 < ueNodes.GetN ())
         {
           clientApps.Add (client.Install (ueNodes.Get(u+1)));
@@ -205,7 +204,7 @@ main (int argc, char *argv[])
         {
           clientApps.Add (client.Install (ueNodes.Get(0)));
         }
-      */
+      
     }
   serverApps.Start (Seconds (0.01));
   clientApps.Start (Seconds (0.01));
