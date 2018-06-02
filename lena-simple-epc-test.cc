5cdfa32c129196b3adddd2ef5cde071935967159
ruccin <hecate4428@daum.net>
2018-06-02 12:48:18 +0900

    fixed

--- lena-simple-epc-test.cc
+++ lena-simple-epc-test.cc
@@ -158,55 +158,27 @@ main (int argc, char *argv[])
   // Install and start applications on UEs and remote host
   uint16_t dlPort = 1234;
   uint16_t ulPort = 2000;
-  uint16_t otherPort = 3000;
+
   ApplicationContainer clientApps;
   ApplicationContainer serverApps;
+
   for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
     {
       ++ulPort;
       ++otherPort;
-      PacketSinkHelper dlPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), dlPort));
-      PacketSinkHelper ulPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), ulPort));
-      PacketSinkHelper packetSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), otherPort));
+      BulkSendHelper dlPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (ueIpIface, dlPort));
+      BulkSendHelper ulPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (remoteHostAddr, ulPort));
+      
+      clientApps.Add (dlPacketSinkHelper.Install (ueNodes.Get(u)));
+      clientApps.SetAttribute ("SendSize", UintegerValue (1024));
+      clientApps.SetAttribute ("MaxBytes", UintegerValue (4294967295u));
 
-      // KwangMin
-
-      BulkSendApplication dlBulkSendApplication ("ns3::BulkSendApplication", InetSocketAddress (Ipv4Address::GetAny (), dlPort));
-      BulkSendApplication ulBulkSendApplication ("ns3::BulkSendApplication", InetSocketAddress (Ipv4Address::GetAny (), ulPort));
-      BulkSendApplication bulkSendApplication ("ns3::BulkSendApplication", InetSocketAddress (Ipv4Address::GetAny (), otherPort));
-
-      dlBulkSendApplication.SetAttribute ("SendSize", UintegerValue(4096));
-      ulBulkSendApplication.SetAttribute ("SendSize", UintegerValue(4096));
-      bulkSendApplication.SetAttribute ("SendSize", UintegerValue(4096));
-
-      // KwangMin
-      serverApps.Add (dlPacketSinkHelper.Install (ueNodes.Get(u)));
       serverApps.Add (ulPacketSinkHelper.Install (remoteHost));
-      serverApps.Add (packetSinkHelper.Install (ueNodes.Get(u)));
+      serverApps.SetAttribute ("SendSize", UintegerValue (1024));
+      serverApps.SetAttribute ("MaxBytes", UintegerValue (4294967295u));
 
-      UdpClientHelper dlClient (ueIpIface.GetAddress (u), dlPort);
-      dlClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
-      dlClient.SetAttribute ("MaxPackets", UintegerValue(4294967295u));
-
-      UdpClientHelper ulClient (remoteHostAddr, ulPort);
-      ulClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
-      ulClient.SetAttribute ("MaxPackets", UintegerValue(4294967295u));
-
-      UdpClientHelper client (ueIpIface.GetAddress (u), otherPort);
-      client.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
-      client.SetAttribute ("MaxPackets", UintegerValue(4294967295u));
-
-      clientApps.Add (dlClient.Install (remoteHost));
-      clientApps.Add (ulClient.Install (ueNodes.Get(u)));
-      if (u+1 < ueNodes.GetN ())
-        {
-          clientApps.Add (client.Install (ueNodes.Get(u+1)));
-        }
-      else
-        {
-          clientApps.Add (client.Install (ueNodes.Get(0)));
-        }
     }
+
   serverApps.Start (Seconds (0.01));
   clientApps.Start (Seconds (0.01));
 
