7069a8283caebcdf1445f42cd058e62f47131d9b
ruccin <Hecate4428@daum,net>
2018-07-12 18:13:32 +0900

    fixed

--- wifi-tcp-test.cc
+++ wifi-tcp-test.cc
@@ -217,10 +217,10 @@ main (int argc, char *argv[])
   staInterface = address.Assign (staDevices);
 
   Ipv4StaticRoutingHelper ipv4RoutingHelper;
-  Ptr<Ipv4StaticRouting> apStaticRouting = Ipv4RoutingHelper.GetStaticRouting (apWifiNode->GetObject<Ipv4> ());
+  Ptr<Ipv4StaticRouting> apStaticRouting = ipv4RoutingHelper.GetStaticRouting (apWifiNode->GetObject<Ipv4> ());
   apStaticRouting->AddHostRouteTo (Ipv4Address ("1.0.0.2"), Ipv4Address ("2.0.0.1"), 1);
 
-  Ptr<Ipv4StaticRouting> SBSStaticRouting = Ipv4RoutingHelper.GetStaticRouting (smallBS->GetObject<Ipv4> ());
+  Ptr<Ipv4StaticRouting> SBSStaticRouting = ipv4RoutingHelper.GetStaticRouting (smallBS->GetObject<Ipv4> ());
   //SBSStaticRouting->AddHostRouteTo (Ipv4Address ("1.0.0.2"), Ipv4Address ("1.0.0.2"), 1);
   SBSStaticRouting->SetDefaultRoute (internetIpIface.GetAddress (1), 1);
 
