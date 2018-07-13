cbf7b24878648d8e927d7943d6c47afbbd7bc46e
ruccin <Hecate4428@daum,net>
2018-07-13 12:58:26 +0900

    fixed

--- wifi-tcp-test.cc
+++ wifi-tcp-test.cc
@@ -188,8 +188,8 @@ main (int argc, char *argv[])
   /* Mobility model */
   MobilityHelper mobility;
   Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-  positionAlloc->Add (Vector (40.0, 0.0, 0.0));
-  positionAlloc->Add (Vector (40.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (20.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (20.0, 0.0, 0.0));
   positionAlloc->Add (Vector (20.0, 0.0, 0.0));
   positionAlloc->Add (Vector (1.0, 0.0, 0.0));
 
