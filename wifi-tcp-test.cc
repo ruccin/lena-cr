3d668606a79a8f2e202e2b0250ce2f783a72ad1d
ruccin <Hecate4428@daum,net>
2018-07-13 12:24:42 +0900

    fixed

--- wifi-tcp-test.cc
+++ wifi-tcp-test.cc
@@ -226,7 +226,7 @@ main (int argc, char *argv[])
   //staStaticRouting->AddHostRouteTo (Ipv4Address ("1.0.0.2"), Ipv4Address ("10.0.0.1"), 1);
 
   Ptr<Ipv4StaticRouting> apStaticRouting = ipv4RoutingHelper.GetStaticRouting (apWifiNode->GetObject<Ipv4> ());
-  apStaticRouting->AddHostRouteTo (csmaIpIface.GetAddress (0), csmaIpIface.GetAddress (0), 1);
+  apStaticRouting->AddNetworkRouteTo (csmaIpIface.GetAddress (0), csmaIpIface.GetAddress (0), 1);
 
   //Ptr<Ipv4StaticRouting> SBSStaticRouting = ipv4RoutingHelper.GetStaticRouting (smallBS->GetObject<Ipv4> ());
   //SBSStaticRouting->AddHostRouteTo (Ipv4Address ("1.0.0.2"), Ipv4Address ("1.0.0.2"), 1);
@@ -241,7 +241,6 @@ main (int argc, char *argv[])
   sink = StaticCast<PacketSink> (sinkApp.Get (0));
 
   /* Install TCP/UDP Transmitter on the station */
-  /*
   OnOffHelper client ("ns3::TcpSocketFactory", (InetSocketAddress (csmaIpIface.GetAddress (0), 9)));
   client.SetAttribute ("PacketSize", UintegerValue (payloadSize));
   client.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
@@ -249,6 +248,7 @@ main (int argc, char *argv[])
   client.SetAttribute ("DataRate", DataRateValue (DataRate (dataRate)));
   ApplicationContainer clientApp = client.Install (staWifiNode);
 
+/*
   Ptr<Ipv4> stack = smallBS->GetObject<Ipv4> ();
   Ptr<Ipv4RoutingProtocol> rp_Gw = (stack->GetRoutingProtocol ());
   Ptr<Ipv4ListRouting> lrp_Gw = DynamicCast<Ipv4ListRouting> (rp_Gw);
@@ -265,11 +265,6 @@ main (int argc, char *argv[])
     }
   }
 */
-  UdpEchoClientHelper echoClient (csmaIpIface.GetAddress (0), 9);
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (1000));
-  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
-  ApplicationContainer clientApp = echoClient.Install (staWifiNode);
 
   /* Start Applications */
   sinkApp.Start (Seconds (0.0));
