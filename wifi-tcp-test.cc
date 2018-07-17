1d735ab7f749fb24cd9289680f99362b1f0f8383
ruccin <Hecate4428@daum,net>
2018-07-17 16:00:50 +0900

    fixed

--- wifi-tcp-test.cc
+++ wifi-tcp-test.cc
@@ -194,7 +194,7 @@ main (int argc, char *argv[])
   wifiPhy.Set ("CcaMode1Threshold", DoubleValue (-79));
   wifiPhy.Set ("EnergyDetectionThreshold", DoubleValue (-79 + 3));
   wifiPhy.SetErrorRateModel ("ns3::YansErrorRateModel");
-  wifiPhy.Set ("ChannelWidth", UintegerValue (20));
+  wifiPhy.SetAttribute ("ChannelWidth", UintegerValue (5));
   wifiHelper.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
                                       "DataMode", StringValue (phyRate),
                                       "ControlMode", StringValue ("HtMcs0"));
