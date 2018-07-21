55c9082842d3616b98ca660a91e630078d7ed4d1
ruccin <Hecate4428@daum,net>
2018-07-21 15:03:54 +0900

    fixed

--- wifi-tcp-test.cc
+++ wifi-tcp-test.cc
@@ -124,8 +124,8 @@ main (int argc, char *argv[])
   Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (payloadSize));
 
   Config::SetDefault ("ns3::ThreeLogDistancePropagationLossModel::Distance0", DoubleValue (0));
-  Config::SetDefault ("ns3::ThreeLogDistancePropagationLossModel::Distance1", DoubleValue (150));
-  Config::SetDefault ("ns3::ThreeLogDistancePropagationLossModel::Distance2", DoubleValue (300));
+  Config::SetDefault ("ns3::ThreeLogDistancePropagationLossModel::Distance1", DoubleValue (50));
+  Config::SetDefault ("ns3::ThreeLogDistancePropagationLossModel::Distance2", DoubleValue (100));
 
   WifiMacHelper wifiMac;
   WifiHelper wifiHelper;
@@ -222,8 +222,8 @@ main (int argc, char *argv[])
   /* Mobility model */
   MobilityHelper mobility;
   Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-  positionAlloc->Add (Vector (300.0, 0.0, 0.0));
-  positionAlloc->Add (Vector (150.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (100.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (50.0, 0.0, 0.0));
   positionAlloc->Add (Vector (0.0, 0.0, 0.0));
 
   mobility.SetPositionAllocator (positionAlloc);
