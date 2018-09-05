0bbafd236860eb1df8f2b3575da9f3003c05e0a2
ruccin <Hecate4428@daum,net>
2018-09-05 14:16:33 +0900

    test

--- ns-3-lbt-31555ff7be4a/src/propagation/bindings/callbacks_list.py
+++
@@ -1,3 +0,0 @@
-callback_classes = [
-    ['double', 'ns3::Ptr<ns3::MobilityModel>', 'ns3::Ptr<ns3::MobilityModel>', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty'],
-]
